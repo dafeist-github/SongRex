3856614027b18d9a18f2b05313ad4972c390de94
DaFeist <56165444+dafeist-github@users.noreply.github.com>
2024-04-01 16:59:33 +0200

    vite config env

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -4,14 +4,6 @@ import replace from '@rollup/plugin-replace';
 export default {
 	kit: {
 		adapter: adapter(),
-			vite: {
-				plugins: [
-				  replace({
-					preventAssignment: true,
-					'process.env.SERVER_HOST': JSON.stringify(process.env.SERVER_HOST || 'http://localhost'),
-				  }),
-				],
-			  },
 	}
 };
 
--- web/vite.config.js
+++ web/vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(),
+		replace({
+			preventAssignment: true,
+			'process.env.SERVER_HOST': JSON.stringify(process.env.SERVER_HOST || 'http://localhost'),
+		  }),
+	]
 });
