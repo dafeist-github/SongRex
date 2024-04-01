384301d3732553c32d159c2c18d9220b41d6c3e8
DaFeist <56165444+dafeist-github@users.noreply.github.com>
2024-04-01 16:57:16 +0200

    use rollup/plugin-replace for ENV-VARIABLES

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,8 +1,17 @@
 import adapter from '@sveltejs/adapter-node';
+import replace from '@rollup/plugin-replace';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+			vite: {
+				plugins: [
+				  replace({
+					preventAssignment: true,
+					'process.env.SERVER_HOST': JSON.stringify(process.env.SERVER_HOST || 'http://localhost'),
+				  }),
+				],
+			  },
 	}
 };
 
