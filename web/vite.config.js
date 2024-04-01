e91e25894b6578dd5d2e5de5160d027e02b2b122
DaFeist <56165444+dafeist-github@users.noreply.github.com>
2024-04-01 18:12:30 +0200

    aa

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import replace from '@rollup/plugin-replace';
+//import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
 	plugins: [sveltekit(),
-		replace({
-			preventAssignment: true,
-			'process.env.SERVER_HOST': JSON.stringify(import.meta.env.VITE_SERVER_HOST || 'http://localhost'),
-		  }),
+		//replace({
+		//	preventAssignment: true,
+		//	'process.env.SERVER_HOST': JSON.stringify(import.meta.env.VITE_SERVER_HOST || 'http://localhost'),
+		//  }),
 	]
 });
