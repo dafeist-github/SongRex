081f95f6c7f8c00435e571e2b9c50fb4edced17d
DaFeist <56165444+dafeist-github@users.noreply.github.com>
2024-04-01 18:03:03 +0200

    update CMD in Dockerfile

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [sveltekit(),
 		replace({
 			preventAssignment: true,
-			'process.env.SERVER_HOST': JSON.stringify(process.env["SERVER_HOST"] || 'http://localhost'),
+			'process.env.SERVER_HOST': JSON.stringify(process.env.SERVER_HOST || 'http://localhost'),
 		  }),
 	]
 });
