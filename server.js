41ed37189d4c0c8289c63abb0f0751e617b9a218
DaFeist <56165444+dafeist-github@users.noreply.github.com>
2024-04-06 16:28:02 +0200

    require handler

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-import { handler } from './svelte/build/handler.js';
+const handler = require('./svelte/build/handler.js');
 
 const argon2 = require("argon2");
 
